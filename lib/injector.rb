3f0787708f64e7be7b986534fb22d1bc60c6c8fe
Ptolemy <ptolemybarnes@gmail.com>
2015-06-24 16:42:35 +0100

    Left inline comments

--- lib/injector.rb
+++ lib/injector.rb
@@ -1,10 +1,12 @@
 class Array
 
-def dave_inject(acc = 0, &block)
-  self.each do |x|
-    acc = block.([acc, x ])
+  def dave_inject(acc = 0, &block)
+:xa
+fg
+    self.each do |x|
+      acc = block.([acc, x])
+    end
+    acc
   end
-  acc
-end
 
 end
--- spec/injector_spec.rb
+++ spec/injector_spec.rb
@@ -4,11 +4,11 @@ describe Array do
 
 context 'dave_inject' do
   it 'addition injection' do
-  expect([1,2,3,4,5].dave_inject(0){|a, b| a + b}).to eq 15
+    expect([1,2,3,4,5].dave_inject(0){|a, b| a + b}).to eq 15 # indentation!
   end
 
   it 'addition injection' do
-  expect([1,2,3,4,5].dave_inject{|a, b| a + b}).to eq 15
+  expect([1,2,3,4,5].dave_inject {|a, b| a + b}).to eq 15
   end
 
   it 'subtraction injection' do
@@ -21,6 +21,10 @@ context 'dave_inject' do
 
   it 'division injection' do
   expect([1,2,3,4].dave_inject(120){|a, b| a / b}).to eq 5
+  end
+
+  it 'takes a symbol' do
+    expect([1,2].dave_inject(:+)).to eq 2 # Try to make this pass!
   end
 end
 
